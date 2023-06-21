fd3cd63e54fab4050f44099b7a3cf85abe17ac8a
Heitor Prado <heitorrdpp@gmail.com>
2023-06-21 17:27:17 -0300

    feat(react): write signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,21 @@
+import '../style/pages/signup.css'
+
 const Signup = () => {
   return (
-    <>
+    <div className="signup-page">
       <h2>Sign Up</h2>
-    </>
+      <form>
+        <div className='container username-container'>
+          <label htmlFor="username">Username</label>
+          <input type="text" name='username'/>
+        </div>
+        <div className='container password-container'>
+          <label htmlFor="password">Password</label>
+          <input type="password" name='password'/>
+        </div>
+        <input type="submit" value="Sign Up"/>
+      </form>
+    </div>
   )
 }
 
