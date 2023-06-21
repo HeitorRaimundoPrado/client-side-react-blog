b251f385fa60b67bca91b7dab07a001a7bcc662f
Heitor Prado <heitorrdpp@gmail.com>
2023-06-21 11:22:37 -0300

    feat(style): basic blog-page styles

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,20 @@
+import '../style/pages/blog.css'
 const Blog = () => {
+  const handleAddNewPost = (e) => {
+    e.preventDefault();
+  }
   return (
-    <>
-      <h2>Blog</h2>
-    </>
+    <div className='blog-page'>
+      <form onSubmit={handleAddNewPost}>
+        <h2 className='child'>Add new Post</h2>
+        <div className='child title-input'>
+          <label htmlFor="title">Title</label>
+          <input type="text" id='title'/>
+        </div>
+        <textarea rows="20" cols="50" className='child'></textarea>
+        <input type="submit" className='child submit-button' value='Create Post'/>
+      </form>
+    </div>
   )
 }
 
